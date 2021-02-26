d985d11f7ed3160e11a72aed2ebc37ee7a92b2ea
AA-Ron <aaron.henderson@rfa.sc.gov>
2021-02-26 14:40:41 -0700

    add users table and registrations controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-		:jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :validatable,
+         :recoverable, :rememberable, :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 end
