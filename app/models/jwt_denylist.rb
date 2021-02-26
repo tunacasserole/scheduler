ee7c494a8387c9646c313a2a1bb1f00699a57cc9
AA-Ron <aaron.henderson@rfa.sc.gov>
2021-02-26 14:27:27 -0700

    fix denylist model

--- app/models/jwt_denylist.rb
+++ app/models/jwt_denylist.rb
@@ -1,9 +1,5 @@
-class CreateJwtDenylist < ActiveRecord::Migration[6.0]
-  def change
-    create_table :jwt_denylist do |t|
-      t.string :jti, null: false
-      t.datetime :expired_at, null: false
-    end
-    add_index :jwt_denylist, :jti
-  end
+class JwtDenylist < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::Denylist
+
+  self.table_name = 'jwt_denylist'
 end
