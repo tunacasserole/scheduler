eab0f715e35cfcdd5be209e97b81ead5983731eb
AA-Ron <aaron.henderson@rfa.sc.gov>
2021-02-27 13:19:04 -0700

    cleanup registrations spec, defer login method

---
+++ spec/controllers/api/sessions_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe SessionsController, type: :request do
+
+  let (:user) { create_user }
+  let (:login_url) { '/api/login' }
+  let (:logout_url) { '/api/logout' }
+
+  context 'When password is missing' do
+    before do
+      post login_url, params: {
+        user: {
+          email: user.email,
+          password: nil
+        }
+      }
+    end
+
+    it 'returns 401' do
+      expect(response.status).to eq(401)
+    end
+
+  end
+
+  context 'When logging out' do
+    it 'returns 204' do
+      delete logout_url
+
+      expect(response).to have_http_status(204)
+    end
+  end
+
+end
